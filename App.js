11303d47462de593d3f61c27b84360af01604521
Sachin Raj <sachintk99@gmail.com>
2023-03-13 12:52:57 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+
+
+const Decker = () => {
+   <div className="Decker">
+     {/* <Header/>
+     <Body/>
+     <Footer/> */}
+   </div>
+};
+
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+
+root.render("Decker")
